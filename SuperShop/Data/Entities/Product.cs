93bb18703263c988215f5f03a09bd8e2a5934c29
Fabíola Martins <fabiola.andrade.martins@outlook.com>
2024-07-22 15:55:41 +0100

    Modify products.

--- SuperShop/Data/Entities/Product.cs
+++ SuperShop/Data/Entities/Product.cs
@@ -8,22 +8,30 @@ namespace SuperShop.Data.Entities
     {
         public int Id { get; set; }
 
+        [Required]
+        [MaxLength(50, ErrorMessage = "The field {0} can contain {1} characters length.")]
         public string Name { get; set; }
+
 
         [DisplayFormat(DataFormatString ="{0:C2}", ApplyFormatInEditMode = false)]
         public decimal Price { get; set; }
 
+
         [Display(Name = "Image")]
         public string ImageUrl {  get; set; }
 
+
         [Display(Name = "Last Purchase")]
-        public DateTime LastPurchase {  get; set; }
+        public DateTime? LastPurchase {  get; set; }
+
 
         [Display(Name = "Last Sale")]
-        public DateTime LastSale { get; set; }
+        public DateTime? LastSale { get; set; }
+
 
         [Display(Name = "Is Avaiable")]
         public bool IsAvaiable { get; set; }
+
 
         [DisplayFormat(DataFormatString = "{0:N2}", ApplyFormatInEditMode = false)]
         public double Stock {  get; set; }
