7c49ea5f3b83128ce7a51e9bbc1d2752f909efd1
Fabíola Martins <fabiola.andrade.martins@outlook.com>
2024-07-23 12:07:43 +0100

    Repository

--- SuperShop/Startup.cs
+++ SuperShop/Startup.cs
@@ -31,9 +31,9 @@ namespace SuperShop
             });
 
             services.AddTransient<SeedDb>();
+            services.AddScoped<IRepository, Repository>();
 
             services.AddControllersWithViews();
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
