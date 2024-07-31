1aeb791c1f1a0f05fec9331a428944ac5b93996d
Fabíola Martins <fabiola.andrade.martins@outlook.com>
2024-07-31 13:00:42 +0100

    Image Helper and ConverterHelper

---
+++ SuperShop/Helpers/ImageHelper.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.IO;
+using System;
+using System.Threading.Tasks;
+
+namespace SuperShop.Helpers
+{
+    public class ImageHelper : IImageHelper
+    {
+        public  async Task<string> UploadImageAsync(IFormFile imagefile, string folder)
+        {
+            string guid = Guid.NewGuid().ToString();
+            string file = $"{guid}.jpg";
+           
+            string path = Path.Combine(
+                Directory.GetCurrentDirectory(),
+                $"wwwroot\\images\\{folder}",
+            file);
+
+            using (FileStream stream = new FileStream(path, FileMode.Create))
+            {
+                await imagefile.CopyToAsync(stream);
+            }
+
+            return $"~/images/{folder}/{file}";
+        }
+    }
+}
--- SuperShop/Startup.cs
+++ SuperShop/Startup.cs
@@ -45,6 +45,11 @@ namespace SuperShop
 
             services.AddScoped<IUserHelper, UserHelper>();
 
+            services.AddScoped<IImageHelper, ImageHelper>();
+
+            services.AddScoped<IConverterHelper, ConverterHelper>();
+            
+
             services.AddScoped<IProductRepository, ProductRepository>();
 
             services.AddControllersWithViews();
