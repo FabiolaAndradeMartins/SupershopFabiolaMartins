d6e00c48e7feb1ecc01240cd6cdbf02083e21918
Fabíola Martins <fabiola.andrade.martins@outlook.com>
2024-09-07 22:05:30 +0100

    Confirm orders

--- SuperShop/Data/Entities/Order.cs
+++ SuperShop/Data/Entities/Order.cs
@@ -12,12 +12,12 @@ namespace SuperShop.Data.Entities
 
         [Required]
         [Display(Name = "Order date")]
-        [DisplayFormat(DataFormatString ="{0:yyyy/mm/dd hh:mm tt}", ApplyFormatInEditMode = false)]
+        [DisplayFormat(DataFormatString ="{0:yyyy/MM/dd hh:mm tt}", ApplyFormatInEditMode = false)]
         public DateTime OrderDate { get; set; }
 
         [Required]
         [Display(Name = "Delivery date")]
-        [DisplayFormat(DataFormatString = "{0:yyyy/mm/dd hh:mm tt}", ApplyFormatInEditMode = false)]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd hh:mm tt}", ApplyFormatInEditMode = false)]
         public DateTime DeliveryDate { get; set; }
 
         [Required]
@@ -25,11 +25,20 @@ namespace SuperShop.Data.Entities
 
         public IEnumerable<OrderDetail> Items { get; set; }
 
+
+        [DisplayFormat(DataFormatString ="{0:n0}")]
+        public int Lines => Items == null ? 0 : Items.Count();
+
+
         [DisplayFormat(DataFormatString = "{0:N2}")]
         public double Quantity => Items == null ? 0 : Items.Sum(i => i.Quantity);
 
         [DisplayFormat(DataFormatString = "{0:C2}")]
         public decimal Value=> Items == null ? 0 : Items.Sum(i => i.Value);
+
+        [Display (Name = "Order date")]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd hh:mm tt}", ApplyFormatInEditMode = false)]
+        public DateTime? OrderDateLocal => this.OrderDate == null ? null : this.OrderDate.ToLocalTime();
 
 
     }
--- SuperShop/Data/IOrderRepository.cs
+++ SuperShop/Data/IOrderRepository.cs
@@ -10,10 +10,16 @@ namespace SuperShop.Data
     public interface IOrderRepository : IGenericRepository<Order>
     {
         Task<IQueryable<Order>> GetOrderAsync(string userName);
+
         Task<IQueryable<OrderDetailTemp>> GetDetailsTempsAsync(string userName);
+
         Task AddItemToOrderAsync(AddItemViewModel model, string userName);
-        Task ModifyOrderDetailTempQuantityAsync(int id, double quantity);        
+
+        Task ModifyOrderDetailTempQuantityAsync(int id, double quantity); 
+        
         Task DeleteDetailTempAsync(int id);
+
+        Task<bool> ConfirmOrderAsync(string userName);
 
     }
 }
