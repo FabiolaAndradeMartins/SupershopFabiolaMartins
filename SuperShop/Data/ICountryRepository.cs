883cffcce512b89f46288e92ae2614d988c4be10
Fabíola Martins <fabiola.andrade.martins@outlook.com>
2024-09-15 17:05:39 +0100

    Datepicker Countries and Cities

--- SuperShop/Data/ICountryRepository.cs
+++ SuperShop/Data/ICountryRepository.cs
@@ -1,9 +1,33 @@
-﻿using SuperShop.Data.Entities;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using SuperShop.Data.Entities;
+using SuperShop.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Threading.Tasks;
 
 namespace SuperShop.Data
 {
     public interface ICountryRepository : IGenericRepository<Country>
     {
+        IQueryable GetCountriesWithCities();
+
+        Task<Country> GetCountryWithCitiesAsync(int id);
+
+        Task<City>GetCityAsync(int id);
+
+        Task AddCityAsync(CityViewModel model);
+
+        Task<int> UpdateCityAsync(City city);
+
+        Task<int> DeleteCityAsync(City city);
+
+        //IEnumerable<SelectListItem> GetComboCountries();
+
+        //IEnumerable<SelectListItem> GetComboCities(int CountryId);
+
+        //Task<Country> GetCountryAsync(City city);
+
 
     }
 }
